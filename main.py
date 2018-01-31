9591e7301e72ff08ea8c8f7f45a1dab35974510f
rogerscristo <rogers.cristo@gmail.com>
2018-01-31 11:01:51 -0200

    new features /lero and /word

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
 
-from random import randint
+import random as rd
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, Job)
 import logging
 
@@ -42,7 +42,7 @@ def help(bot, update):
 
 def random(bot, update):
     # update.message.reply_text('Em manutenção :(')
-    messageID = randint(6, 150)
+    messageID = rd.randint(6, 175)
     try:
         # bot.forwardMessage(update.message.chat_id, '@ofwdnovo', messageID)
         # -- Linha de debug para verificar se o fwd está funcionando
@@ -56,7 +56,164 @@ def debug(bot, update):
 
     print("Chat id %d" % update.message.chat_id) 
     print("Message id %d" % update.message.message_id) 
-    bot.forwardMessage(update.message.chat_id, "@botFwdTeste", 10)
+    for x in range(0, 175):
+        try:
+            # bot.forwardMessage(update.message.chat_id, '@ofwdnovo', messageID)
+            # -- Linha de debug para verificar se o fwd está funcionando
+            bot.forwardMessage(update.message.chat_id, '@botFwdTeste', x)
+            print("Success => message_id %d" % x)
+        except:
+            print("Error => message_id %d does not exist" % x)
+            continue
+
+def lero(bot, update):
+    t0 = [  'Caros amigos, ',
+	'Por outro lado, ',
+	'Assim mesmo, ',
+	'No entanto, não podemos esquecer que ',
+	'Do mesmo modo, ',
+	'A prática cotidiana prova que ',
+	'Nunca é demais lembrar o peso e o significado destes problemas, uma vez que ',
+	'As experiências acumuladas demonstram que ',
+	'Acima de tudo, é fundamental ressaltar que ',
+	'O incentivo ao avanço tecnológico, assim como ',
+	'Não obstante, ',
+	'Todas estas questões, devidamente ponderadas, levantam dúvidas sobre se ',
+	'Pensando mais a longo prazo, ',
+	'O que temos que ter sempre em mente é que ',
+	'Ainda assim, existem dúvidas a respeito de como ',
+	'Gostaria de enfatizar que ',
+	'Todavia, ',
+	'A nível organizacional, ',
+	'O empenho em analisar ',
+	'Percebemos, cada vez mais, que ',
+	'No mundo atual, ',
+	'É importante questionar o quanto ',
+	'Neste sentido, ',
+	'Evidentemente, ',
+	'Por conseguinte, ',
+	'É claro que ',
+	'Podemos já vislumbrar o modo pelo qual ',
+	'Desta maneira, ',
+	'O cuidado em identificar pontos críticos n',
+	'A certificação de metodologias que nos auxiliam a lidar com ' ]
+
+    t1 = [	'a execução dos pontos do programa ',
+            'a complexidade dos estudos efetuados ',
+            'a contínua expansão de nossa atividade ',
+            'a estrutura atual da organização ',
+            'o novo modelo estrutural aqui preconizado ',
+            'o desenvolvimento contínuo de distintas formas de atuação ',
+            'a constante divulgação das informações ',
+            'a consolidação das estruturas ',
+            'a consulta aos diversos militantes ',
+            'o início da atividade geral de formação de atitudes ',
+            'o desafiador cenário globalizado ',
+            'a mobilidade dos capitais internacionais ',
+            'o fenômeno da Internet ',
+            'a hegemonia do ambiente político ',
+            'a expansão dos mercados mundiais ',
+            'o aumento do diálogo entre os diferentes setores produtivos ',
+            'a crescente influência da mídia ',
+            'a necessidade de renovação processual ',
+            'a competitividade nas transações comerciais ',
+            'o surgimento do comércio virtual ',
+            'a revolução dos costumes ',
+            'o acompanhamento das preferências de consumo ',
+            'o comprometimento entre as equipes ',
+            'a determinação clara de objetivos ',
+            'a adoção de políticas descentralizadoras ',
+            'a valorização de fatores subjetivos ',
+            'a percepção das dificuldades ',
+            'o entendimento das metas propostas ',
+            'o consenso sobre a necessidade de qualificação ',
+            'o julgamento imparcial das eventualidades ' ]
+
+    t2 = [  'nos obriga à análise ',
+            'cumpre um papel essencial na formulação ',
+            'exige a precisão e a definição ',
+            'auxilia a preparação e a composição ',
+            'garante a contribuição de um grupo importante na determinação ',
+            'assume importantes posições no estabelecimento ',
+            'facilita a criação ',
+            'obstaculiza a apreciação da importância ',
+            'oferece uma interessante oportunidade para verificação ',
+            'acarreta um processo de reformulação e modernização ',
+            'pode nos levar a considerar a reestruturação ',
+            'representa uma abertura para a melhoria ',
+            'ainda não demonstrou convincentemente que vai participar na mudança ',
+            'talvez venha a ressaltar a relatividade ',
+            'prepara-nos para enfrentar situações atípicas decorrentes ',
+            'maximiza as possibilidades por conta ',
+            'desafia a capacidade de equalização ',
+            'agrega valor ao estabelecimento ',
+            'é uma das consequências ',
+            'promove a alavancagem ',
+            'não pode mais se dissociar ',
+            'possibilita uma melhor visão global ',
+            'estimula a padronização ',
+            'aponta para a melhoria ',
+            'faz parte de um processo de gerenciamento ',
+            'causa impacto indireto na reavaliação ',
+            'apresenta tendências no sentido de aprovar a manutenção ',
+            'estende o alcance e a importância ',
+            'deve passar por modificações independentemente ',
+            'afeta positivamente a correta previsão ' ]
+
+
+    t3 = [  'das condições financeiras e administrativas exigidas.',
+            'das diretrizes de desenvolvimento para o futuro.',
+            'do sistema de participação geral.',
+            'das posturas dos órgãos dirigentes com relação às suas atribuições.',
+            'das novas proposições.',
+            'das direções preferenciais no sentido do progresso.',
+            'do sistema de formação de quadros que corresponde às necessidades.',
+            'das condições inegavelmente apropriadas.',
+            'dos índices pretendidos.',
+            'das formas de ação.',
+            'dos paradigmas corporativos.',
+            'dos relacionamentos verticais entre as hierarquias.',
+            'do processo de comunicação como um todo.',
+            'dos métodos utilizados na avaliação de resultados.',
+            'de todos os recursos funcionais envolvidos.',
+            'dos níveis de motivação departamental.',
+            'da gestão inovadora da qual fazemos parte.',
+            'dos modos de operação convencionais.',
+            'de alternativas às soluções ortodoxas.',
+            'dos procedimentos normalmente adotados.',
+            'dos conhecimentos estratégicos para atingir a excelência.',
+            'do fluxo de informações.',
+            'do levantamento das variáveis envolvidas.',
+            'das diversas correntes de pensamento.',
+            'do impacto na agilidade decisória.',
+            'das regras de conduta normativas.',
+            'do orçamento setorial.',
+            'do retorno esperado a longo prazo.',
+            'do investimento em reciclagem técnica.',
+            'do remanejamento dos quadros funcionais.' ]
+    leroLero = rd.choice(t0)+rd.choice(t1)+rd.choice(t2)+rd.choice(t3)
+    bot.send_message(chat_id=update.message.chat_id, 
+                     text=leroLero)
+
+def word(bot, update):
+    consonants = 'bcdfghjlmnpqrstvxz'
+    vowels = 'aeiou'
+    syllables = rd.randint(2, 10)
+    result = ''
+    for i in range(syllables):
+        consonant = rd.choice(consonants)
+        this_vowels = vowels
+        if consonant == 'q':
+            consonant += 'u'
+            this_vowels = 'aeio'
+        result += consonant + rd.choice(this_vowels)
+        if i > 0 and result[-2] in 'bp' and rd.randint(0, 5) == 0:
+            result = result[:-2] + 'm' + result[-2:]
+        if i > 0 and result[-2] in 'cglrst' and rd.randint(0, 5) == 0:
+            result = result[:-2] + 'n' + result[-2:]
+    bot.send_message(chat_id=update.message.chat_id, 
+                     text=result)
+    
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
@@ -73,6 +230,11 @@ def main():
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("random", random))
+    dp.add_handler(CommandHandler("fwd", random))
+    dp.add_handler(CommandHandler("lero", lero))
+    dp.add_handler(CommandHandler("lerolero", lero))
+    dp.add_handler(CommandHandler("word", word))
+    dp.add_handler(CommandHandler("palavra", word))
     dp.add_handler(CommandHandler("debug", debug))
 
     # log all errors
